81426202843ed3d990bf706428cddc96c5c7e7f2
Mitja <mitja.kurki@gmail.com>
2018-08-23 16:27:50 -0400

    py file

---
+++ scripts/scrape_vcf_info.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+import argparse
+import gzip
+import re
+
+def read_info_fields(file):
+    fields = []
+    for line in file:
+        if line.startswith("#CHROM"):
+            break
+        if line.startswith("##INFO=<"):
+            line = line.rstrip("\n")
+            dat = { elem[0]:elem[1] for elem in map(lambda x: x.split("="), re.sub("^##INFO=<|>$","",line).split(",")) }
+            fields.append(dat)
+
+    return fields
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Export info fields as TSV file')
+    parser.add_argument('vcf', type=str)
+    parser.add_argument('outfile', type=str)
+
+    args = parser.parse_args()
+
+    with open(args.outfile, 'w') as out:
+        with gzip.open(args.vcf,mode='rt') as infile:
+            fields = read_info_fields(infile)
+            fields.sort( key=lambda x: x["ID"]  )
+
+            items = map(lambda x: x["ID"], fields )
+
+            out.write( "variant\tchr\tpos" + "\t".join(items) + "\n" )
+
+            for line in infile:
+                dat = line.split("\t")[0:8]
+                var = dat[0] + "_" + dat[1] + "_" + dat[3] + "_" + dat[4]
+                info = { d[0]:(d[1] if len(d)>1 else "") for d in list(map(lambda x: x.split("="), dat[7].split(";")) ) }
+
+                def getdat( elem, info):
+                    if elem["Type"]=="Flag":
+                        return "1" if elem["ID"] in info else "0"
+                    else:
+                        return info[elem["ID"]] if elem["ID"] in info else "NA"
+
+                out.write( var + "\t" + dat[0] + "\t" + dat[1] + "\t".join([  getdat(elem, info) for elem in fields  ] ) + "\n")
