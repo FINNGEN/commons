9d6396cc226a0bae8651c40e5a7352f1333a94a6
juhaa <juha.mehtonen@outlook.com>
2020-11-02 08:06:07 +0000

    Mimic joinsort from META_ANALYSIS

--- liftover/scripts/joinsort.sh
+++ liftover/scripts/joinsort.sh
@@ -3,13 +3,13 @@
 ##     @script.name [option] ARGUMENTS...
 ##
 ## Options:
-##     -h, --help              All client scripts have this, it can be omitted.
-##     --var=VALUE        index for variant column chr:pos:ref:alt. This or next four must be specified
-##     --chr=VALUE        Columnn indexes
+##     -h, --help       All client scripts have this, it can be omitted.
+##     --var=VALUE      Index for variant column chr:pos:ref:alt. This or next four must be specified
+##     --chr=VALUE      Columnn indexes
 ##     --pos=VALUE
 ##     --ref=VALUE
 ##     --alt=VALUE
-##     --sep=VALUE       column separator in data. if you want space give 'space' as easyoptions does not accept space as cmdline parameter
+##     --tmp=VALUE      Specify tmp directory location for sorting and joining...
 
 
 
@@ -30,15 +30,11 @@ fi
 
 cols=$( $cat_cmd < "$inputfile" | head -n 1 | awk 'BEGIN {FS="\t"}{ print NF}')
 
+temploc="-T /tmp/"
 
-if [[ ! -n "$sep"  ]]
+if [[ -n "$tmp" ]]
 then
-    ## tried to add optional sep but could not figure out how to get proper tab character to variable to be used below
-    ## this does not work
-    colsep=$'-t\t'
-else
-    sep=${sep/space/ /}
-    cat_cmd=$cat_cmd" |  tr "$sep" '\t'"
+	temploc="-T $tmp"
 fi
 
 if [[ ! -n "$var"  ]]
@@ -49,15 +45,13 @@ then
         exit 1
     fi
     cols=$((cols+1))
-    cat <( $cat_cmd < "$inputfile" | awk -v var=$var 'BEGIN {FS="\t"}  NR==1{ printf "#variant"; for(i=1;i<=NF; i++) if(i!=var) printf "\t"$i; printf "\tlift_chr\tlift_pos\tREF\tALT" ;printf "\n"; exit 0 }'  )  \
-        <( join -1 1 -2 3 -t$'\t' -a 1 <(  $cat_cmd < "$inputfile" | awk -v chr=$chr -v pos=$pos -v ref=$ref -v alt=$alt ' BEGIN{OFS="\t"; FS="\t"} NR>1{ print $chr":"$pos":"$ref":"$alt,$0 }' | sort -b -k 1,1 -t $'\t'  ) <( awk 'BEGIN{ OFS="\t"; } { print $1,$2+1,$4}'  $liftedfile | sort -t$'\t' -b -k 3,3 ) \
-| awk -v n_exp_col=$((cols+2)) 'BEGIN{ FS="\t"; OFS="\t"} { split($1,a,":"); printf $0;  if (NF<n_exp_col) { printf "\tNA\t0"; for(i=NF+2;i<n_exp_col;i++) {printf "\tNA" } };  printf "\t"a[3]"\t"a[4]"\n"; } ' | sort -t$'\t' -V -k $((cols+1)),$((cols+1))  -k $((cols+2)),$((cols+2)) ) |  bgzip > $(basename $inputfile)".lifted.gz"
+    join -1 1 -2 3 -t$'\t' <(  $cat_cmd < "$inputfile" | awk -v chr=$chr -v pos=$pos -v ref=$ref -v alt=$alt ' BEGIN{OFS="\t"} NR==1{ print "#variant",$0 } NR>1{ print $chr":"$pos":"$ref":"$alt,$0 }' | sort -b -k 1,1 -t $'\t' $temploc ) \
+    <( awk 'BEGIN{ OFS="\t"; print "anew_chr","anew_pos","#variant" }{ print $1,$2+1,$4}' $liftedfile | sort $temploc -t$'\t' -b -k 3,3 ) \
+    | sort $temploc -t$'\t' -V -k $((cols+1)),$((cols+1)) -k $((cols+2)),$((cols+2)) | awk 'BEGIN{ FS="\t"; OFS="\t"} NR==1{ print $0,"REF","ALT"} NR>1{ split($1,a,":"); print $0,a[3],a[4] } '|  bgzip > $(basename $inputfile)".lifted.gz"
 else
-
-cat <( $cat_cmd < "$inputfile" | awk -v var=$var 'BEGIN {FS="\t"}  NR==1{ printf "#variant"; for(i=1;i<=NF; i++) if(i!=var) printf "\t"$i; printf "\tlift_chr\tlift_pos\tREF\tALT" ;printf "\n"; exit 0 }'  )  \
-    <(join -1 1 -2 3 -t$'\t' -a 1 <(  $cat_cmd < "$inputfile" | awk -v var=$var 'BEGIN {FS="\t"}  NR>1{ printf $var; for(i=1;i<=NF; i++) if(i!=var) printf "\t"$i; printf "\n"; }' | sort -b -k 1,1 -t $'\t'  ) \
-        <( awk 'BEGIN{ OFS="\t";}{ print $1,$2+1,$4}'  $liftedfile | sort -t$'\t' -b -k 3,3 )  \
-        | awk -v n_exp_col=$((cols+3)) 'BEGIN{ FS="\t"; OFS="\t"} { split($1,a,":"); printf $0;  if (NF<n_exp_col) { printf "\tNA\t0"; for(i=NF+2;i<=exp_col;i++) printf "\tNA" }  printf "\t"a[3]"\t"a[4]"\n" } ' | sort -t$'\t' -V -k $((cols+1)),$((cols+1))  -k $((cols+2)),$((cols+2)) ) |  bgzip > $(basename $inputfile)".lifted.gz"
+    join -1 1 -2 3 -t$'\t' <(  $cat_cmd < "$inputfile" | awk -v var=$var 'NR==1{ printf "#variant"; for(i=1;i<=NF; i++) if(i!=var) printf "\t"$i; printf "\n"; } NR>1{ printf $var; for(i=1;i<=NF; i++) if(i!=var) printf "\t"$i; printf "\n"; }' | sort $temp_loc -b -k 1,1 -t $'\t'  ) \
+    <( awk 'BEGIN{ OFS="\t"; print "anew_chr","anew_pos","#variant" }{ print $1,$2+1,$4}'  $liftedfile | sort $temp_loc -t$'\t' -b -k 3,3 ) \
+    | sort $temp_loc -t$'\t' -V -k $((cols+1)),$((cols+1))  -k $((cols+2)),$((cols+2)) | awk  'BEGIN{ FS="\t"; OFS="\t"} NR==1{ print $0,"REF","ALT"} NR>1{ split($1,a,":"); print $0,a[3],a[4] } '|  bgzip > $(basename $inputfile)".lifted.gz"
 fi
 
 tabix -s $((cols+1)) -b $((cols+2)) -e $((cols+2)) $(basename $inputfile)".lifted.gz"
