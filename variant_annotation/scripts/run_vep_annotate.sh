4e2aad762ef3bb3ac6c2d81b08a1328d0abcf679
Juha Karjalainen <jkarjala@broadinstitute.org>
2022-04-13 13:19:19 +0300

    give vep json bucket location as the default is requester pays now

--- variant_annotation/scripts/run_vep_annotate.sh
+++ variant_annotation/scripts/run_vep_annotate.sh
@@ -19,6 +19,9 @@ hailctl dataproc start $clustername --vep $build --region $region --requester-pa
 # note that double wildcards ** dont work here
 vcf_in="gs://finngen-imputation-panel/sisu4/vcf/snpid/*.vcf.gz"
 out_prefix="gs://finngen-imputation-panel/sisu4/hail/sisu4_annot"
+#chip:
+#vcf_in="gs://r9-data/chip/vcf/r9_axiom_chip_chr*_subset.vcf.gz"
+#out_prefix="gs://r9-data/chip/anno/r9_chip_vep_annot_update"
 
 ## submit annotation job
 #
@@ -28,4 +31,5 @@ out_prefix="gs://finngen-imputation-panel/sisu4/hail/sisu4_annot"
 # while running, uncomment hl._set_flags(no_whole_stage_codegen='1') in vep_annotate.py
 #
 hailctl dataproc submit $clustername --region $region --pyfiles $(dirname -- "$0")/hail_functions.py \
-	$(dirname -- "$0")/vep_annotate.py $vcf_in $out_prefix --overwrite
+	$(dirname -- "$0")/vep_annotate.py $vcf_in $out_prefix --overwrite \
+	--vep_conf gs://r9-data/vep95-GRCh38-loftee-gcloud.json
