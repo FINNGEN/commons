995b3df233cb35a4464b73167abdcbce2a8af959
hafa786 <hafizm.shehzad@gmail.com>
2025-06-09 09:54:37 +0300

    rename the variable names

--- convert_wide_to_long_format/convert_wide_to_long_format.py
+++ convert_wide_to_long_format/convert_wide_to_long_format.py
@@ -22,7 +22,7 @@ def run(input_file, output_file, columns):
             # Read the first line (header)
             print('start reading rows')
             with gzip.open(input_file, 'rt') as file:
-                header = file.readline().strip().split('\t')
+                header_line = file.readline().strip().split('\t')
                 provided_columns = columns.replace("|", "\t")
                 output_file_header = f"{FINNGENID}\t{COLUMN}\t{VALUE}\t{provided_columns}\n"
                 output.write(output_file_header)
@@ -30,22 +30,22 @@ def run(input_file, output_file, columns):
                 # reading input file line
                 for line in file:
                     row = line.strip().split('\t')
-                    for head in header:
+                    for header_key in header_line:
                         resulted_row = []
-                        if header.index(head) == 0:
-                            finn_gen_id = row[header.index(head)]
+                        if header_line.index(header_key) == 0:
+                            finn_gen_id = row[header_line.index(header_key)]
                             continue
                         else:
                             resulted_row.append(finn_gen_id)
-                            resulted_row.append(f"{head}")
-                            resulted_row.append(row[header.index(head)])
+                            resulted_row.append(f"{header_key}")
+                            resulted_row.append(row[header_line.index(header_key)])
                             for suffix in sufficies:
-                                prefix_column = f"{head}_{suffix}"
-                                if prefix_column in header:
-                                    resulted_row.append(row[header.index(prefix_column)])
+                                prefix_column = f"{header_key}_{suffix}"
+                                if prefix_column in header_line:
+                                    resulted_row.append(row[header_line.index(prefix_column)])
                         if (len(resulted_row) == len(output_file_header.split('\t'))):
                            output.write(f"\t".join(resulted_row)+"\n")
-                        print_progress_bar(header.index(head), len(header) + 1, prefix='Progress', suffix=f"FINNGENID {row[0]} Complete", length=40)
+                        print_progress_bar(header_line.index(header_key), len(header_line) + 1, prefix='Progress', suffix=f"FINNGENID {row[0]} Complete", length=40)
         print('\nFinished successfully!')
     except Exception as e:  # Catch any exception
         print(f"An error occurred: {e}")
