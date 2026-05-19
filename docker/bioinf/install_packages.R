e213444f0372cf9a784b04e00c50ee10ea008ee7
juhaa <juhaa@users.noreply.github.com>
2026-05-19 18:19:26 +0000

    Make R package install fail on unsuccesfull installation; Add duckdbfs; Fix bioconductor packages

--- docker/bioinf/install_packages.R
+++ docker/bioinf/install_packages.R
@@ -1,6 +1,13 @@
 #!/usr/bin/env Rscript
 
-req_packages <- c(
+# R 4.2.3 bundles Matrix 1.5-3 but several packages require >= 1.6.0.
+# Current CRAN Matrix requires R > 4.2.3, so install last compatible version from archive.
+install.packages(
+  "https://cran.r-project.org/src/contrib/Archive/Matrix/Matrix_1.6-5.tar.gz",
+  repos = NULL, type = "source"
+)
+
+cran_packages <- c(
 "data.table",
 "tidyverse",
 "rjson",
@@ -15,14 +22,43 @@ req_packages <- c(
 "janitor",
 "tidylog",
 "RNOmni",
-"rtracklayer",
 "viridis",
 "CMplot",
 "patchwork",
-"OmicCircos")
+"duckdb",
+"duckdbfs"
+)
 
-for (pack in req_packages) {
-    if(!require(pack,character.only = TRUE)) {
+bioc_packages <- c(
+"rtracklayer",
+"OmicCircos"
+)
+
+install_cran <- function(pack) {
+    if (!require(pack, character.only = TRUE, quietly = TRUE)) {
         install.packages(pack, repos = "http://cran.us.r-project.org")
+        if (!require(pack, character.only = TRUE, quietly = TRUE)) {
+            stop(paste("Failed to install CRAN package:", pack))
+        }
     }
+}
+
+install_bioc <- function(pack) {
+    if (!require(pack, character.only = TRUE, quietly = TRUE)) {
+        if (!requireNamespace("BiocManager", quietly = TRUE)) {
+            install.packages("BiocManager", repos = "http://cran.us.r-project.org")
+        }
+        BiocManager::install(pack, ask = FALSE)
+        if (!require(pack, character.only = TRUE, quietly = TRUE)) {
+            stop(paste("Failed to install Bioconductor package:", pack))
+        }
+    }
+}
+
+for (pack in cran_packages) {
+    install_cran(pack)
+}
+
+for (pack in bioc_packages) {
+    install_bioc(pack)
 }
